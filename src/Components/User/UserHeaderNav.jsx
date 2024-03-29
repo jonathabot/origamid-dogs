28692b56b3e8278fd4e1fefad817121e33e07586
Jonatha <lebotelhon@gmail.com>
2024-03-29 03:23:15 -0300

    user header

---
+++ src/Components/User/UserHeaderNav.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from "react";
+import { NavLink, Navigate } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import MinhasFotos from "../../Assets/feed.svg?react";
+import Estatisticas from "../../Assets/estatisticas.svg?react";
+import AdicionarFoto from "../../Assets/adicionar.svg?react";
+import Sair from "../../Assets/sair.svg?react";
+import styles from "./UserHeaderNav.module.css";
+import { useNavigate } from "react-router-dom";
+
+const UserHeaderNav = () => {
+  const [mobile, setMobile] = useState(null);
+  const { userLogout } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    userLogout();
+    navigate("/login");
+  }
+
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MinhasFotos />
+        {mobile && "Minhas Fotos"}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        {mobile && "Estatísticas"}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AdicionarFoto />
+        {mobile && "Adicionar Foto"}
+      </NavLink>
+      <button onClick={handleLogout}>
+        <Sair />
+        {mobile && "Sair"}
+      </button>
+    </nav>
+  );
+};
+
+export default UserHeaderNav;
